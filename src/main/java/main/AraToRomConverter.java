82790e3fdfe01d563cf5e70fc4329c9ed377100a
job-lukasz <job.lukasz.1990@gmail.com>
2014-05-05 14:17:36 +0200

    Code refactor- create TreeMap of converting symbols Arabic (integer) Roman (string) and change findeMaxRomanNumber method; add initIterator method

--- src/main/java/main/AraToRomConverter.java
+++ src/main/java/main/AraToRomConverter.java
@@ -1,30 +1,48 @@
 package main;
 
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class AraToRomConverter {
 	private int araNumber;
 	
-	
-	public String convert(int arabicNumber){
+	private static final Map<Integer, String> numberMap;
+    static {
+        Map<Integer, String> tempMap = new TreeMap<Integer, String>(java.util.Collections.reverseOrder());
+        tempMap.put(10, "X");
+        tempMap.put(5, "V");
+        tempMap.put(1, "I");
+        numberMap = Collections.unmodifiableMap(tempMap);
+    }
+    
+    private Iterator<Integer> arabicNumberSetIt;
+    private int currentRomanNumber;
+    
+	public String convert(int numberToConvert){
 		String result = "";
-		araNumber = arabicNumber;
+		araNumber = numberToConvert;
+		initIterator();
 		while(araNumber>0){
 			result+=findMaxRomanNumber();
 		}
 		return result;
 	}
 	
+	private void initIterator() {
+		arabicNumberSetIt = numberMap.keySet().iterator();
+		currentRomanNumber = arabicNumberSetIt.next();
+	}
+
 	private String findMaxRomanNumber(){
-		if(araNumber>=10){
-			araNumber-=10;
-			return "X";
+		if(currentRomanNumber<=araNumber){
+			araNumber-=currentRomanNumber;
+			return numberMap.get(currentRomanNumber);
 		}
-		else if(araNumber>=5){
-			araNumber-=5;
-			return "V";
+		if(arabicNumberSetIt.hasNext()){
+			currentRomanNumber = arabicNumberSetIt.next();
 		}
-		else{
-			araNumber--;
-			return "I";
-		}
+		return "";
 	}
 }
