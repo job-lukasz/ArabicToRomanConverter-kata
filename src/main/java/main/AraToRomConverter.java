7c3d8ffa47f0a93b7636b4c52315063b244de120
job-lukasz <job.lukasz.1990@gmail.com>
2014-05-05 13:52:36 +0200

    Add functionality of converting 5 to V

--- src/main/java/main/AraToRomConverter.java
+++ src/main/java/main/AraToRomConverter.java
@@ -4,8 +4,14 @@ public class AraToRomConverter {
 	public String convert(int arabicNumber){
 		String result = "";
 		while(arabicNumber>0){
-			result += "I";
-			arabicNumber--;
+			if(arabicNumber>=5){
+				result+="V";
+				arabicNumber-=5;
+			}
+			else{
+				result += "I";
+				arabicNumber--;
+			}
 		}
 		return result;
 	}
--- src/test/java/main/RomanNumericTest.java
+++ src/test/java/main/RomanNumericTest.java
@@ -5,22 +5,18 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class RomanNumericTest {
-  @DataProvider(name = "TestingNumbers")
+	@DataProvider(name = "TestingNumbers")
 	public Object[][] numbersArabicRoman() {
-	 return new Object[][] {
-	   { new Integer(0), "" },
-	   { new Integer(1), "I" },
-	   { new Integer(2), "II" },
-	 };
+		return new Object[][] { { new Integer(0), "" }, { new Integer(1), "I" }, { new Integer(2), "II" }, { new Integer(5), "V" }, };
 	}
 
-  @Test(dataProvider = "TestingNumbers")
-  public void ArabicNumberToRomanString(int arabic, String roman) {
-	  //Arrange
-	  AraToRomConverter converter = new AraToRomConverter();
-	  //Act
-	  String result = converter.convert(arabic);
-	  //Assert
-	  Assert.assertEquals(result, roman);
-  }
+	@Test(dataProvider = "TestingNumbers")
+	public void ArabicNumberToRomanString(int arabic, String roman) {
+		// Arrange
+		AraToRomConverter converter = new AraToRomConverter();
+		// Act
+		String result = converter.convert(arabic);
+		// Assert
+		Assert.assertEquals(result, roman);
+	}
 }
