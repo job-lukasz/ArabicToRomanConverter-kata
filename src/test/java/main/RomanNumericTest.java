33829178849a85fe6d5b919e32695efc80902600
job-lukasz <job.lukasz.1990@gmail.com>
2014-05-06 10:53:24 +0200

    test code refactor - one data provider for both tests

--- src/test/java/main/RomanNumericTest.java
+++ src/test/java/main/RomanNumericTest.java
@@ -5,37 +5,30 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class RomanNumericTest {
-	@DataProvider(name = "ArabToRomanTestingNumbers")
+	@DataProvider(name = "TestingNumbers")
 	public Object[][] numbersArabicRoman() {
-		return new Object[][] { { new Integer(0), "" }, { new Integer(1), "I" }, { new Integer(2), "II" }, { new Integer(4), "IV" },
-				{ new Integer(5), "V" }, { new Integer(9), "IX" }, { new Integer(10), "X" }, { new Integer(40), "XL" },
-				{ new Integer(50), "L" }, { new Integer(90), "XC" }, { new Integer(100), "C" }, { new Integer(400), "CD" },
-				{ new Integer(500), "D" }, { new Integer(900), "CM" }, { new Integer(1000), "M" }, { new Integer(1999), "MCMXCIX" },
-				{ new Integer(3448), "MMMCDXLVIII" }, { new Integer(1684), "MDCLXXXIV" }, };
+		return new Object[][] { { new Integer(0), "" }, 
+								 { new Integer(1), "I" }, 
+								 { new Integer(2), "II" }, 
+								 { new Integer(4), "IV" },
+								 { new Integer(5), "V" }, 
+								 { new Integer(9), "IX" },
+								 { new Integer(10), "X" },
+								 { new Integer(40), "XL" },
+								 { new Integer(50), "L" }, 
+								 { new Integer(90), "XC" },
+								 { new Integer(100), "C" },
+								 { new Integer(400), "CD" },
+								 { new Integer(500), "D" },
+								 { new Integer(900), "CM" },
+								 { new Integer(1000), "M" },
+								 { new Integer(1999), "MCMXCIX" },
+								 { new Integer(3448), "MMMCDXLVIII" },
+								 { new Integer(1684), "MDCLXXXIV" },
+								};
 	}
 
-	@DataProvider(name = "RomanToArabTestingNumbers")
-	public Object[][] numbersRomanArabic() {
-		return new Object[][] { { "", new Integer(0) }, 
-								 { "I", new Integer(1) }, 
-								 { "II", new Integer(2) },
-								 { "V", new Integer(5)},
-								 { "X", new Integer(10)},
-								 { "L", new Integer(50)},
-								 { "C", new Integer(100)},
-								 { "D", new Integer(500)},
-								 { "M", new Integer(1000)},
-								 { "IV", new Integer(4)},
-								 { "IX", new Integer(9)},
-								 { "XL", new Integer(40)},
-								 { "XC", new Integer(90)},
-								 { "CD", new Integer(400)},
-								 { "CM", new Integer(900)},
-								 { "MMMCDXLVIII",new Integer(3448) }
-								 };
-	}
-
-	@Test(dataProvider = "ArabToRomanTestingNumbers")
+	@Test(dataProvider = "TestingNumbers")
 	public void ArabicNumberToRomanString(int arabic, String roman) {
 		// Arrange
 		AraToRomConverter converter = new AraToRomConverter();
@@ -45,8 +38,8 @@ public class RomanNumericTest {
 		Assert.assertEquals(result, roman);
 	}
 
-	@Test(dataProvider = "RomanToArabTestingNumbers")
-	public void RomanStringToArabNumber(String roman, int arabic) {
+	@Test(dataProvider = "TestingNumbers")
+	public void RomanStringToArabNumber(int arabic, String roman) {
 		// Arrange
 		RomToAraConverter converter = new RomToAraConverter();
 		// Act
