affa10ab544019a993847fa3410a30ad65ac4f0f
job-lukasz <job.lukasz.1990@gmail.com>
2014-05-06 10:50:18 +0200

    code refactor - add array with convertings strings

--- src/main/java/main/RomToAraConverter.java
+++ src/main/java/main/RomToAraConverter.java
@@ -1,20 +1,24 @@
 package main;
 
 public class RomToAraConverter {
+	public static final String convertersString[][] = { { "CM", "DCD" }, 
+														   { "M", "DD" }, 
+														   { "CD", "CCCC" },
+														   { "D", "CCCCC" },
+														   { "XC", "LXL" }, 
+														   { "C", "LL" }, 
+														   { "XL", "XXXX" },
+														   { "L", "XXXXX" },
+														   { "IX", "VIV" },
+														   { "X", "VV" },
+														   { "IV", "IIII" },
+														   { "V", "IIIII" } 
+														   };
+
 	public int convert(String romanNumber) {
-		romanNumber=romanNumber.replaceAll("CM", "DCD");
-		romanNumber=romanNumber.replaceAll("M", "DD");
-		romanNumber=romanNumber.replaceAll("CD", "CCCC");
-		romanNumber=romanNumber.replaceAll("D", "CCCCC");
-		romanNumber=romanNumber.replaceAll("XC", "LXL");
-		romanNumber=romanNumber.replaceAll("C", "LL");
-		romanNumber=romanNumber.replaceAll("XL", "XXXX");
-		romanNumber=romanNumber.replaceAll("L", "XXXXX");
-		romanNumber=romanNumber.replaceAll("IX", "VIV");
-		romanNumber=romanNumber.replaceAll("X", "VV");
-		romanNumber=romanNumber.replaceAll("IV", "IIII");
-		romanNumber=romanNumber.replaceAll("V", "IIIII");
-		
+		for (int curConvStrNum = 0; curConvStrNum < 12; curConvStrNum++) {
+			romanNumber = romanNumber.replaceAll(convertersString[curConvStrNum][0], convertersString[curConvStrNum][1]);
+		}
 		return romanNumber.length();
 	}
 }
